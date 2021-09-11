f01f69383baf993b8814b8cc8f2febc4179394b2
EgorEast <jir.jir35@gmail.com>
2021-09-11 18:06:17 +0300

    component DetailsPage is realized

--- client/src/pages/DetailPage.js
+++ client/src/pages/DetailPage.js
@@ -1,9 +1,30 @@
-import React from 'react';
+import React, { useContext, useEffect, useState } from 'react';
+import { useParams } from 'react-router';
+import { LinkCard } from '../components/LinkCard';
+import { Loader } from '../components/Loader';
+import { AuthContext } from '../context/AuthContext';
+import { useHttp } from '../hooks/http.hook';
 
 export const DetailPage = () => {
-	return (
-		<div>
-			<h1>Detail Page</h1>
-		</div>
-	);
+	const { token } = useContext(AuthContext);
+	const { request, loading } = useHttp();
+	const [link, setLink] = useState(null);
+	const linkId = useParams().id;
+
+	useEffect(async () => {
+		try {
+			const fetched = await request(`/api/link/${linkId}`, 'GET', null, {
+				authorization: `Bearer ${token}`,
+			});
+			setLink(fetched);
+		} catch (e) {
+			// empty
+		}
+	}, [token, linkId, request]);
+
+	if (loading) {
+		return <Loader />;
+	}
+
+	return <>{!loading && link && <LinkCard link={link} />}</>;
 };
